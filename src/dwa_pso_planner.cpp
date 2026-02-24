db79e8c5b992efe1adee055d1b11ea2b52c000ac
ktiggos <k.tiggos@gmail.com>
2026-02-24 16:08:14 +0200

    dwa_pso: Zero init cmd_vel prior to pso

--- src/dwa_pso_planner.cpp
+++ src/dwa_pso_planner.cpp
@@ -63,6 +63,15 @@ void DwaPsoPlanner::plannerCB()
 
     geometry_msgs::msg::Twist cmd_vel;
 
+    // Init zero cmd before computation
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.linear.y = 0.0;
+    cmd_vel.linear.z = 0.0;
+
+    cmd_vel.angular.x = 0.0;
+    cmd_vel.angular.y = 0.0;
+    cmd_vel.angular.z = 0.0;
+
     cmd_vel = this->pso_optimize_cmd(odom, wnd);
 
     // Optional safety: keep command inside window bounds
