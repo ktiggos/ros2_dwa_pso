94ebe653aad56788acf8c771804a5be25b2807c6
ktiggos <k.tiggos@gmail.com>
2026-05-27 17:23:46 +0300

    planner_metrics: Use dfs and update cmd_vel

--- scripts/planner_metrics.py
+++ scripts/planner_metrics.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import pandas as pd
+
 import rclpy
 
 from rclpy.node import Node
@@ -13,7 +15,12 @@ class PlannerMetrics(Node):
         self.odom_msg = Odometry()
         self.cmd_msg = Twist()
 
-        self.path = list()
+        self.path_data = pd.DataFrame(columns=['x','y'])
+        self.time_data = pd.DataFrame(columns=['stamp', 'vel_trans', 'vel_ang'])
+
+        self.cmd_count = 0
+        self.odom_count = 0
+        self.csv_path = "../metrics/"
 
         self.odom_sub  = self.create_subscription(
             Odometry,
@@ -32,10 +39,15 @@ class PlannerMetrics(Node):
             1.0,
             self.UpdateOdomCB
         )
+
+        self.update_cmd_timer = self.create_timer(
+            1.0,
+            self.UpdateCmdCB
+        )
         
-    
     def __del__(self):
-        print(self.path)
+        pass
+        
     
     def odomCB(self, msg: Odometry):
         self.odom_msg = msg
@@ -49,16 +61,36 @@ class PlannerMetrics(Node):
 
         IS_POSE_NOT_ZERO = (x > 1e-3) or (y > 1e-3)
 
-        if (not self.path) and IS_POSE_NOT_ZERO:
-            self.path.append((x,y))
+        if (self.path_data.shape[0] == 0) and IS_POSE_NOT_ZERO:
+            self.path_data.loc[self.odom_count] = [x,y]
+            print(f"\nWRITE ODOM: ({x},{y})")
+            print(f"ODOM SIZE: {self.path_data.shape[0]}x{self.path_data.shape[1]}\n")
+            self.odom_count += 1
         elif IS_POSE_NOT_ZERO:
-            eps_x = abs(x - self.path[-1][0])
-            eps_y = abs(y - self.path[-1][1])
+            eps_x = abs(x - self.path_data.iloc[-1,0])
+            eps_y = abs(y - self.path_data.iloc[-1,1])
 
-            IS_NOT_STAT = (eps_x > 1e-3) and (eps_y > 1e-3)
+            IS_NOT_STAT = (eps_x > 1e-3) or (eps_y > 1e-3)
 
             if IS_NOT_STAT:
-                self.path.append(x,y)
+                self.path_data.loc[self.odom_count] = [x,y]
+                print(f"\nWRITE ODOM: ({x},{y})")
+                print(f"ODOM SIZE: {self.path_data.shape[0]}x{self.path_data.shape[1]}\n")
+                self.odom_count += 1
+
+    def UpdateCmdCB(self):
+        v = self.cmd_msg.linear.x
+        omega = self.cmd_msg.angular.z
+
+        if v > 1e-3:
+            self.time_data.loc[self.cmd_count] = [
+                self.get_clock().now(),
+                v,
+                omega
+            ]
+            print(f"\nWRITE CMD: ({v},{omega})")
+            print(f"CMD SIZE: {self.time_data.shape[0]}x{self.time_data.shape[1]}\n")
+            self.cmd_count += 1
 
 if __name__ == "__main__":
     rclpy.init(args=None)
