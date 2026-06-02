ed939f12aed8f111cd8e685adddbef92389b1441
ktiggos <k.tiggos@gmail.com>
2026-06-02 22:31:27 +0300

    metrics_wrapper: Wrap also planner metrics

--- ros2_dwa_pso_planner/scripts/metrics_wrapper.py
+++ ros2_dwa_pso_planner/scripts/metrics_wrapper.py
@@ -7,6 +7,7 @@ import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
+from planner_interfaces.msg import Metrics
 
 from ament_index_python import get_package_prefix
 
@@ -15,20 +16,22 @@ class PlannerMetrics(Node):
     def __init__(self):
         super().__init__('planner_metrics')
 
-        prefix = Path(get_package_prefix('ros2_dwa_pso'))
+        prefix = Path(get_package_prefix('ros2_dwa_pso'))        
         prefix_ws = prefix.parent.parent
 
-        self.csv_path = prefix_ws / 'src' / 'ros2_dwa_pso' / 'metrics'
+        self.csv_path = prefix_ws / 'src' / 'ros2_dwa_pso'/ 'ros2_dwa_pso_planner' / 'metrics'
         self.csv_path.mkdir(parents=True, exist_ok=True)
 
         self.odom_msg = Odometry()
         self.cmd_msg = Twist()
+        self.metrics_msg = Metrics()
 
         self.path_data = pd.DataFrame(columns=['x','y'])
-        self.time_data = pd.DataFrame(columns=['stamp', 'vel_trans', 'vel_ang'])
+        self.timed_data = pd.DataFrame(columns=['stamp', 'vel_trans', 'vel_ang', 'comp_time', 'robot_clearence'])
 
-        self.cmd_count = 0
+        self.timed_count = 0
         self.odom_count = 0
+        self.metrics_count = 0
         self.update_time = 0.5
 
         self.odom_sub  = self.create_subscription(
@@ -43,15 +46,20 @@ class PlannerMetrics(Node):
             self.cmdCB,
             10
         )
+        self.metrics_sub = self.create_subscription(
+            Metrics,
+            'planner_metrics',
+            self.metricsCB,
+            10
+        )
 
         self.update_odom_timer = self.create_timer(
             self.update_time,
             self.UpdateOdomCB
         )
-
         self.update_cmd_timer = self.create_timer(
             self.update_time,
-            self.UpdateCmdCB
+            self.UpdateTimedDataCB
         )
     
     def odomCB(self, msg: Odometry):
@@ -59,6 +67,9 @@ class PlannerMetrics(Node):
 
     def cmdCB(self, msg: Twist):
         self.cmd_msg = msg
+
+    def metricsCB(self, msg: Metrics):
+        self.metrics_msg = msg
 
     def UpdateOdomCB(self):
         x = self.odom_msg.pose.pose.position.x
@@ -83,29 +94,33 @@ class PlannerMetrics(Node):
                 print(f"ODOM SIZE: {self.path_data.shape[0]}x{self.path_data.shape[1]}\n")
                 self.odom_count += 1
 
-    def UpdateCmdCB(self):
+    def UpdateTimedDataCB(self):
         v = self.cmd_msg.linear.x
         omega = self.cmd_msg.angular.z
+        comp_time = self.metrics_msg.comp_time
+        robot_clear = self.metrics_msg.robot_clearence
 
         stamp = self.get_clock().now().nanoseconds * 1e-9
-        
 
         if v > 1e-3:
-            self.time_data.loc[self.cmd_count] = [
+            self.timed_data.loc[self.timed_count] = [
                 stamp,
                 v,
-                omega
+                omega,
+                comp_time,
+                robot_clear
             ]
             print(f"\nWRITE CMD: ({v},{omega})")
-            print(f"CMD SIZE: {self.time_data.shape[0]}x{self.time_data.shape[1]}\n")
-            self.cmd_count += 1
+            print(f"WRITE METRICS: comp_time = {comp_time}, clear = {robot_clear}")
+            print(f"TIMED DATA SIZE: {self.timed_data.shape[0]}x{self.timed_data.shape[1]}\n")
+            self.timed_count += 1
 
     def save_data(self):
         self.path_data.to_csv(self.csv_path / "path_data.csv",
                               encoding='utf-8',
                               index=False
         )
-        self.time_data.to_csv(self.csv_path / "time_data.csv",
+        self.timed_data.to_csv(self.csv_path / "timed_data.csv",
                               encoding='utf-8',
                               index=False
         )
